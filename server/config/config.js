e73b6137849d8ff2c37fcf3ca60589d9fbdb760d
root <root@debian2>
2019-12-25 22:59:12 -0600

    Coneccion a la BBDD cifrada

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://leoromero:BolonYcol4ever@leoromeromdb-vztlw.mongodb.net/test?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
